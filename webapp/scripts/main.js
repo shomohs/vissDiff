1cb73f0d3a41c1a46c4b1ff03d0ebf0ccc30794e
Abhinav Chadha <abhinav.chadha9@gmail.com>
2015-05-14 21:07:10 +0200

    removed the tacky table and made it look simple and beautiful. added a dummy button

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,12 +1,18 @@
 $( window ).load( function() {
     console.log( "ready!" );
+
+    $( '#diff' ).on( 'click', function() {
+    	console.log( 'clicked on DIFF' );
+    	alert( "DIFF!" );
+    });
+
     $( '#submit-1' ).on( 'click', function() {
-		console.log( 'submit 1 clicked' + $( '#url-1' ).val() );
+		console.log( 'submit 1 clicked, url: ' + $( '#url-1' ).val() );
 		$( '#iframe-1' ).attr( 'src', $( '#url-1' ).val() );
 	});
 
 	$( '#submit-2' ).on( 'click', function() {
-		console.log( 'submit 2 clicked' + $( '#url-2' ).val());
+		console.log( 'submit 2 clicked, url: ' + $( '#url-2' ).val());
 		$( '#iframe-2' ).attr( 'src', $( '#url-2' ).val());
 	});
 });
